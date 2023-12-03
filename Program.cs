cd9f8d5260428c045ee40fd010843c0159e203a6
piotr <piotr@PITUPITU>
2023-12-03 01:05:18 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+
+
+
+namespace _23AdventD1
+{
+    public class dayOne23
+    {
+        public static void Main()
+        {
+            List<String[]> input = ReadFile.getList("23D1");
+            List<int> index = new List<int>();
+            int addNums = 0;
+            string convert;
+            string makeNum;
+            char[] numChar;
+            string[] dictionary = new string[] {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+            string[] translator = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+            for (int i = 0; i < input.Count; i++)
+            {
+                convert = String.Join("", input[i]);
+                numChar = convert.ToCharArray();
+                for (int j = 0; j < numChar.Length; j++)
+                {
+                    for (int d = 0; d < dictionary.Length; d++)
+                    {
+                        if (convert.Contains(dictionary[d], StringComparison.OrdinalIgnoreCase))
+                        {
+                            convert = convert.Insert(convert.IndexOf(dictionary[d]) + 1, translator[d]);
+                        }
+                    }
+                }
+                numChar = convert.ToCharArray();
+                for (int j = 0; j < numChar.Length; j++)
+                {
+                    if (char.IsNumber(numChar[j]))
+                    {   
+                        index.Add(j);
+                    }
+                }
+                makeNum = numChar[index.Min()].ToString() + numChar[index.Max()].ToString();
+                addNums = addNums + Int32.Parse(makeNum);
+                index.Clear();
+            }
+            Console.WriteLine(addNums.ToString());
+
+        }
+
+
+    }
+}
---
+++ ReadFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23AdventD1
+{
+    internal class ReadFile
+    {
+        public static List<string[]> getList(string dName)
+        {
+            string path = @"c:\AdventOfCode\" + dName + ".txt";
+            string[] text = File.ReadAllLines(path);
+            List<string[]> strLines = new List<string[]>();
+            for (int i = 0; i < text.Length; i++)
+            {
+
+                strLines.Add(text[i].Split(' '));
+
+            }
+            return strLines;
+        }
+    }
+}
